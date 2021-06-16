39e5519586a557b1e59451a56f8e9934c18d8659
Jair Botello <jairbotello25@outlook.com>
2021-06-16 15:34:48 -0500

    Pantalla Principal Responsiva

--- app/src/main/java/com/example/gestordeinventario/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/gestordeinventario/AdminSQLiteOpenHelper.java
@@ -11,13 +11,13 @@ public class AdminSQLiteOpenHelper  extends SQLiteOpenHelper {
     }
 
     public void onCreate(SQLiteDatabase BaseDeDatos){
-        BaseDeDatos.execSQL("CREATE TABLE categoria (clave_categoria INTEGER PRIMARY KEY AUTOINCREMENT,nombre TEXT NOT NULL, descripcion TEXT NOT NULL)");
-        BaseDeDatos.execSQL("CREATE TABLE marca (clave_marca INTEGER PRIMARY KEY UNIQUE NOT NULL,nombre TEXT NOT NULL)");
-
+        BaseDeDatos.execSQL("CREATE TABLE categorias (clave_categoria INTEGER PRIMARY KEY AUTOINCREMENT,nombre TEXT NOT NULL, descripcion TEXT NOT NULL)");
+        BaseDeDatos.execSQL("CREATE TABLE marcas (clave_marca INTEGER PRIMARY KEY AUTOINCREMENT,nombre TEXT NOT NULL)");
+        BaseDeDatos.execSQL("CREATE TABLE productos (clave_producto INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT NOT NULL, detalle TEXT NOT NULL, cantidad integer Not NULL, precio real NOT NULL, clave_marca integer, clave_categoria integer, FOREIGN KEY (clave_marca) REFERENCES marcas (clave_marca), FOREIGN KEY (clave_categoria) REFERENCES categorias (clave_categoria))");
     }
 
     public void onOpen (SQLiteDatabase BaseDeDatos){
-        BaseDeDatos.execSQL("PRAGMA foreign_kets=ON");
+        BaseDeDatos.execSQL("PRAGMA foreign_keys=ON");
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
