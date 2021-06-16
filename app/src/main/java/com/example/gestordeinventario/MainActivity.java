245354e9c13e86f73e859db1b7ba131e8ed9f18b
Jair Botello <jairbotello25@outlook.com>
2021-06-16 02:18:17 -0500

    Pruebas SQLite3

--- app/src/main/java/com/example/gestordeinventario/MainActivity.java
+++ app/src/main/java/com/example/gestordeinventario/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gestordeinventario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cargar();
+    }
+
+    public void cargar(){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "inventario",null,1);
+        SQLiteDatabase baseDeDatos = admin.getReadableDatabase();
     }
 }
