e762ab363d7a25f8dcd806df95620c8be535642a
Jair Botello <jairbotello25@outlook.com>
2021-06-17 06:17:27 -0500

    Consultar Detalles y Actualizar Productos

--- app/src/main/java/com/example/gestordeinventario/MainActivity.java
+++ app/src/main/java/com/example/gestordeinventario/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gestordeinventario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
