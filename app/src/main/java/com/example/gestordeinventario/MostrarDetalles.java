a5935af8d1e269d87505e4d88dce08598b85581f
Jair Botello <jairbotello25@outlook.com>
2021-06-17 08:19:15 -0500

    Release v0.1

--- app/src/main/java/com/example/gestordeinventario/MostrarDetalles.java
+++ app/src/main/java/com/example/gestordeinventario/MostrarDetalles.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
