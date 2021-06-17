b9ee35130292674214fe5d939d2f1645b87cb4d2
Jair Botello <jairbotello25@outlook.com>
2021-06-17 02:01:31 -0500

    Productos, Agregar Productos y pantallas reesponsivas

--- app/src/main/java/com/example/gestordeinventario/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/gestordeinventario/AdminSQLiteOpenHelper.java
@@ -11,13 +11,28 @@ public class AdminSQLiteOpenHelper  extends SQLiteOpenHelper {
     }
 
     public void onCreate(SQLiteDatabase BaseDeDatos){
+        //Creamos las tablas que vamos a ocupar
         BaseDeDatos.execSQL("CREATE TABLE categorias (clave_categoria INTEGER PRIMARY KEY AUTOINCREMENT,nombre TEXT NOT NULL, descripcion TEXT NOT NULL)");
         BaseDeDatos.execSQL("CREATE TABLE marcas (clave_marca INTEGER PRIMARY KEY AUTOINCREMENT,nombre TEXT NOT NULL)");
         BaseDeDatos.execSQL("CREATE TABLE productos (clave_producto INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT NOT NULL, detalle TEXT NOT NULL, cantidad integer Not NULL, precio real NOT NULL, clave_marca integer, clave_categoria integer, FOREIGN KEY (clave_marca) REFERENCES marcas (clave_marca), FOREIGN KEY (clave_categoria) REFERENCES categorias (clave_categoria))");
+
+        //Introducimos registros iniciales a las tablas antes creadas para poder inicializar las primary key autoincrement
+        BaseDeDatos.execSQL("INSERT INTO categorias(clave_categoria,nombre,descripcion) VALUES (1,'Bebidas','Refresco, Agua y Té')");
+        BaseDeDatos.execSQL("INSERT INTO categorias(nombre,descripcion) VALUES ('Botanas','Papas,Cacahuates y Frutos Secos')");
+        BaseDeDatos.execSQL("INSERT INTO categorias(nombre,descripcion) VALUES ('Lacteos','Leche,Huevos y Quesos')");
+        BaseDeDatos.execSQL("INSERT INTO categorias(nombre,descripcion) VALUES ('Farmacia','Vitaminas y Cuidado Personal')");
+
+        BaseDeDatos.execSQL("INSERT INTO marcas(clave_marca,nombre) VALUES (1,'Coca-Cola')");
+        BaseDeDatos.execSQL("INSERT INTO marcas(nombre) VALUES ('Sabritas')");
+
+        BaseDeDatos.execSQL("INSERT INTO productos(clave_producto,nombre, detalle,cantidad,precio,clave_marca,clave_categoria) VALUES (700,'Coca-Cola', '600ml',10,13.5,1,1)");
+
     }
 
     public void onOpen (SQLiteDatabase BaseDeDatos){
+        //Activamos las foreign keys ya que en SQLite vienen desactivadas por defecto
         BaseDeDatos.execSQL("PRAGMA foreign_keys=ON");
+
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
---
+++ app/src/main/java/com/example/gestordeinventario/AgregarProducto.java
@@ -0,0 +1,185 @@
+package com.example.gestordeinventario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class AgregarProducto extends AppCompatActivity {
+
+    private Spinner sMarcas, sCategorias;
+    private int posicionMarca, posicionCategoria;
+    private EditText nombreProducto, detalleProducto, cantidadProducto, precioProducto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_producto);
+
+        nombreProducto = findViewById(R.id.nombreProducto);
+        detalleProducto = findViewById(R.id.detallesProducto);
+        cantidadProducto = findViewById(R.id.cantidadProducto);
+        precioProducto = findViewById(R.id.precioProducto);
+
+        sMarcas = findViewById(R.id.spinnerMarcas);
+
+        sMarcas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                posicionMarca = parent.getSelectedItemPosition() ;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        sCategorias = findViewById(R.id.spinnerCategorias);
+
+        sCategorias.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                posicionCategoria = parent.getSelectedItemPosition() ;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        cargarMarcas();
+        cargarCategorias();
+    }
+
+    public void cargarMarcas(){
+        ArrayList<String> listaMarcas = new ArrayList<>();
+        listaMarcas.add("Selecciona");
+
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"inventario",null, 1);
+        SQLiteDatabase BaseDeDatos = admin.getReadableDatabase();
+
+        Cursor cursorMarcas = BaseDeDatos.rawQuery("SELECT clave_marca,nombre FROM marcas",null);
+
+        if(cursorMarcas.moveToFirst()){
+            do{
+                String reg = cursorMarcas.getString(1);
+                listaMarcas.add(reg);
+            }while(cursorMarcas.moveToNext());
+        }
+
+        BaseDeDatos.close();
+        cursorMarcas.close();
+
+        ArrayAdapter <String> adapterMarcas = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item,listaMarcas);
+        sMarcas.setAdapter(adapterMarcas);
+    }
+
+    public void cargarCategorias() {
+        ArrayList<String> listaCategorias = new ArrayList<>();
+        listaCategorias.add("Selecciona");
+
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "inventario", null, 1);
+        SQLiteDatabase BaseDeDatos = admin.getReadableDatabase();
+
+        Cursor cursorCategorias = BaseDeDatos.rawQuery("SELECT clave_categoria,nombre FROM categorias", null);
+
+        if (cursorCategorias.moveToFirst()) {
+            do {
+                String reg = cursorCategorias.getString(1);
+                listaCategorias.add(reg);
+            } while (cursorCategorias.moveToNext());
+        }
+
+        BaseDeDatos.close();
+        cursorCategorias.close();
+
+        ArrayAdapter<String> adapterMarcas = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, listaCategorias);
+        sCategorias.setAdapter(adapterMarcas);
+    }
+
+    public void cancelarAgregar(View view){
+        Intent productos = new Intent(this, Productos.class);
+        startActivity(productos);
+    }
+
+    public void guardarAgregar(View view){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "inventario", null, 1);
+        SQLiteDatabase BaseDeDatos = admin.getWritableDatabase();
+
+
+        String nombre = nombreProducto.getText().toString();
+        String detalle = detalleProducto.getText().toString();
+        String cantidad = cantidadProducto.getText().toString();
+        String precio = precioProducto.getText().toString();
+        int marca = posicionMarca;
+        int categoria = posicionCategoria;
+
+        if(nombre.isEmpty()){
+            Toast.makeText(this, "No dejes vacio el campo de nombre",Toast.LENGTH_SHORT).show();
+        }
+        else if (detalle.isEmpty()){
+            Toast.makeText(this, "No dejes vacio el campo de detalles",Toast.LENGTH_SHORT).show();
+        }
+        else if (cantidad.isEmpty()){
+            Toast.makeText(this, "No dejes vacio el campo de cantidad",Toast.LENGTH_SHORT).show();
+        }
+        else if (precio.isEmpty()){
+            Toast.makeText(this, "No dejes vacio el campo de precio",Toast.LENGTH_SHORT).show();
+        }
+        else if (marca == 0){
+            Toast.makeText(this, "Selecciona una Marca",Toast.LENGTH_SHORT).show();
+        }
+        else if (categoria == 0){
+            Toast.makeText(this, "Selecciona una Categoria",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            int cant = Integer.parseInt(cantidad);
+            float pre = Float.parseFloat(precio);
+
+            ContentValues registro = new ContentValues();
+
+            registro.put("nombre", nombre);
+            registro.put("detalle", detalle);
+            registro.put("cantidad", cant);
+            registro.put("precio", pre);
+            registro.put("clave_marca", marca);
+            registro.put("clave_categoria", categoria);
+
+            BaseDeDatos.insert("productos", null, registro);
+            BaseDeDatos.close();
+
+            Toast.makeText( this, "Producto Guardado", Toast.LENGTH_SHORT).show();
+            limpiar();
+            regresarProductos();
+        }
+    }
+
+    public void limpiar(){
+        nombreProducto.setText("");
+        detalleProducto.setText("");
+        cantidadProducto.setText("");
+        precioProducto.setText("");
+
+        sMarcas.setSelection(0);
+        sCategorias.setSelection(0);
+    }
+
+    public void regresarProductos(){
+        Intent listaProductos = new Intent(this, Productos.class);
+        startActivity(listaProductos);
+    }
+
+
+}
